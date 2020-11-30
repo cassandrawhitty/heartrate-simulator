ddfa3f69232a19b5fdb80860edd9d6ba25bdb04f
cassandrawhitty <cassandraallexwhitty@gmail.com>
2020-11-30 10:55:38 -0700

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Animal.destroy_all
+
+Animal.create(name: "Blue Whales",image: "https://th.bing.com/th/id/OIP.ME295uUdqtTjw7ZwJ3frlwHaD4?w=298&h=180&c=7&o=5&dpr=2&pid=1.7", heartrate: 8)
+Animal.create(name: "Elephants",image: "https://th.bing.com/th/id/OIP.3LU41mFZnO9eh7PLV1ThgAHaE7?w=278&h=184&c=7&o=5&dpr=2&pid=1.7", heartrate: 30)
+Animal.create(name: "Horses",image: "https://th.bing.com/th/id/OIP.hRGSpXMHkMG2ADxKWeZhZAHaEK?w=312&h=180&c=7&o=5&dpr=2&pid=1.7", heartrate: 36)
+Animal.create(name: "Humans",image: "https://i.imgflip.com/39osmx.png", heartrate: 75)
+Animal.create(name: "Big Dogs",image: "https://th.bing.com/th/id/OIP.tSLWAqhEIu_mwWFikxVtcwHaEK?w=316&h=180&c=7&o=5&dpr=2&pid=1.7", heartrate: 80)
+Animal.create(name: "Small Dogs",image: "https://th.bing.com/th/id/OIP.kb7kDexw-WoUxYKR3aFa-gHaE8?w=245&h=180&c=7&o=5&dpr=2&pid=1.7", heartrate: 120)
+Animal.create(name: "Giraffes",image: "https://th.bing.com/th/id/OIP.RmfDzrBrKJq-VFSOWpaIfQHaFS?w=275&h=196&c=7&o=5&dpr=2&pid=1.7", heartrate: 150)
+Animal.create(name: "Cats",image: "https://th.bing.com/th/id/OIP.2xtbyxEs4kpIPIo33MotmQHaEo?w=305&h=190&c=7&o=5&dpr=2&pid=1.7", heartrate: 160)
+Animal.create(name: "Hamsters",image: "https://th.bing.com/th/id/OIP.lbb9le-fgYUMcB5uS1XXUQHaE8?w=253&h=180&c=7&o=5&dpr=2&pid=1.7", heartrate: 400)
+Animal.create(name: "Hummingbirds",image: "https://th.bing.com/th/id/OIP.tAnZUqcvmm7130m0dHWoYgHaE8?w=241&h=180&c=7&o=5&dpr=2&pid=1.7", heartrate: 1200)
+Animal.create(name: "Armadillos",image: "https://th.bing.com/th/id/OIP.DTtpYeDD4sswbRoFQUgBDwHaFV?w=260&h=187&c=7&o=5&dpr=2&pid=1.7", heartrate: 95)
+Animal.create(name: "Sloths",image: "https://th.bing.com/th/id/OIP.BLIH7sOToN_5joQQktY5OwHaEf?w=286&h=180&c=7&o=5&dpr=2&pid=1.7", heartrate: 81)
+Animal.create(name: "Octopuses",image: "https://th.bing.com/th/id/OIP.VaMfu4jwfBZpMJlC4bO10QHaD9?w=334&h=180&c=7&o=5&dpr=2&pid=1.7", heartrate: 45)
+Animal.create(name: "Alligators",image: "https://th.bing.com/th/id/OIP.g5Rzfboys0SE0RGVOS82iAHaEU?w=283&h=180&c=7&o=5&dpr=2&pid=1.7", heartrate: 27)
